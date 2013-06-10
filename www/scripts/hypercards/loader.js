f81ae21aa34ab70f8b631c23999689dcaa5636fa
OpenShift guest <ernestwond521@gmail.com>
2013-06-10 06:42:38 -0400

    stuffed files with stuff (navigational scripts, etc.)

--- www/scripts/hypercards/loader.js
+++ www/scripts/hypercards/loader.js
@@ -1,3 +1,64 @@
-define( function(){
+define( ["json"], function(JSON){
+
+    var fileRequest = window.XMLHttpRequest? new XMLHttpRequest() : window.ActiveXObject? new ActiveXObject("MicrosoftXMLHTTP") : null,
+        protocol = location.protocol,
+        baseUrl = document.baseURI,
+        lessBaseUrl = baseUrl.substr( protocol.length + 2 ),
+        proto;
+    
+    if (!fileRequest) {
+        return false;
+    }
+    
+    ////
+    function Loader() {}
+    proto = Loader.prototype;
+    proto.getCard = getCard;
+    proto.loadFile = loadFile;
+    ////
+    
+    function getCard(id, callback) {
+        var url,
+            idStart,
+            start,
+            end;
+        
+        if (id.substr(-1) === "/") {
+            id = id.slice(0, -1);
+        }
+        idStart = id.indexOf(lessBaseUrl);
+        start = end = idStart === -1? 0 : idStart + lessBaseUrl.length;
+        if (id.charAt(end) === "/") {
+            end++;
+        }
+        
+        loadFile( url, (function createCallback(index) {
+            return function (response) {
+                if (response.status === 404 && !index) {
+                    url += url.substr(-1) === "/"? "index.json" : "/index.json";
+                    loadFile( url, createCallback( true ));
+                }
+                response.data.id = id;
+                response.data.url = url;
+                callback(response);
+            };
+        }));
+    }
+    
+    function loadFile(url, callback) {
+        fileRequest.open("get", url, true);
+        fileRequest.onreadystatechange = function() {
+            if (fileRequest.readyState === 4) {
+                callback({
+                    "data": fileRequest.status === 200? JSON.parse(fileRequest.responseText) : null,
+                    "status": fileRequest.status,
+                    "statusText": fileRequest.statusText
+                });
+            }
+        };
+        fileRequest.send();
+    }
+    
+    return new Loader();
 
 } );
---
+++ www/scripts/hypercards/show.js
@@ -0,0 +1,155 @@
+define( ["marked"], function (marked) {
+    
+    var proto;
+    
+    ////
+    function show() {}
+    proto = show.prototype;
+    proto.display = display;
+    ////
+    
+    marked.setOptions({
+        gfm: false
+    });
+    
+    function display(data,container) {
+        container.innerHTML = dataToHTML(data);
+    }
+    
+    function idToHTML(id) {
+        if (id.charAt(0) === "/") {
+            id = id.slice(1);
+        }
+        var path = id.split("/"),
+            i = path.length,
+            out = [];
+        while (--i >= 0) {
+            out.unshift([
+                "<a href=\"",
+                path.join("/"),
+                "\">",
+                path[i],
+                "</a>\n"].join(""));
+        }
+        return out.join(" > "); //replace with separating character
+    }
+    
+    function linkListToHTML(list) {
+        var i = 0,
+            len = list.length,
+            out = ["<ul>\n"];
+        
+        for ( ; i < len; i++ ) {
+            out = out.concat( [
+                "\n<li>\n<a href=\"",
+                list[i][1],
+                "\">",
+                list[i][0],
+                "</a>\n</li>\n"] );
+        }
+        out.push("\n</ul>");
+        return out.join("");
+    }
+    
+    function listToHTML(list) {
+        var len = list.length;
+        return len > 1? [
+                list.slice(0, len - 1).join(", "),
+                list[len - 1]
+            ].join(" and ") : ( len? "" + list[0] : "");
+    }
+    
+    function dataToHTML(data) {
+        return ["<article>\n<header>\n<div class=\"Hyper-Heading\">\n",
+        
+                data.id && ["<div class=\"Hyper-Path\">\n",
+                        idToHTML(data.id),
+                        "\n</div>\n"
+                    ].join(""),
+                
+                data.title && [
+                        "<h1 class=\"Hyper-Title\">\n",
+                        data.title,
+                        "\n</h1>\n"
+                    ].join(""),
+                
+                "</div>\n</header>\n",
+                
+                data.content && [
+                        "\n<div class=\"Hyper-Content\">\n",
+                        marked(data.content),
+                        "\n</div>\n"
+                    ].join(""),
+                
+                data.seeAlso && [
+                        "\n<section>\n<h2>See Also</h2>\n<div class=\"Hyper-SeeAlso\">\n",
+                        linkListToHTML(data.seeAlso),
+                        "\n</div>\n</section>\n"
+                    ].join(""),
+                    
+                data.sideLinks && [
+                        "\n<div class=\"Hyper-SideLinks\">\n",
+                        linkListToHTML(data.sideLinks),
+                        "\n</div>\n"
+                    ].join(""),
+                
+                "\n<footer>\n<div class=\"Hyper-Footer\">\n",
+                
+                data.authors && [
+                        "\n<div class=\"left\">\nWritten by <span class=\"Hyper-Author\">",
+                        listToHTML(data.authors),
+                        "</span>.\n</div>\n"
+                    ].join(""),
+                
+                data.lastModified && [
+                        "\n<div class=\"right\">\nLast Modified: <time class=\"Hyper-Date\">",
+                        data.lastModified,
+                        "</time>\n</div>\n"
+                    ].join(""),
+                
+                data.url && [
+                        "\n<div class=\"url\">\nJSON: <a class=\"Hyper-Url\" href=\"",
+                        data.url,
+                        "\">",
+                        data.url,
+                        "</a>\n</div>\n"
+                    ].join(""),
+                
+                "\n</div>\n</footer>"
+                
+            ].join("");
+    }
+    
+    /* Format
+    <div id="HyperCard-0" class="Hyper-Container">
+			<article>
+
+				<header>
+					<div class="Hyper-Heading">
+						<div class="Hyper-Path">&nbsp;</div>
+						<h1 class="Hyper-Title">&nbsp;</h1>
+					</div>
+				</header>
+
+				<div class="Hyper-Content">&nbsp;</div>
+
+				<section>
+					<h2>See Also</h2>
+					<div class="Hyper-SeeAlso">&nbsp;</div>
+				</section>
+
+				<div class="Hyper-SideLinks">&nbsp;</div>
+
+				<footer>
+					<div class="Hyper-Footer">
+						<div class="left">Written by <span class="Hyper-Author"></span>.<div>
+						<div class="right">Last Modified: <time class="Hyper-Date"></time></div>
+						<div class="url">JSON: <a class="Hyper-Url" href=""></a></div>
+					</div>
+				</footer>
+
+			</article>
+		</div>
+    */
+    
+});
